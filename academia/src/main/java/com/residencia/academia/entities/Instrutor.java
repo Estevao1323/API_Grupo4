47254670e16b79346c5c4321d9140edfd3de498d
caiquelms <caique.lemosferreira7@gmail.com>
2023-10-11 21:05:01 -0300

    entities instrutor e telefone

--- academia/src/main/java/com/residencia/academia/entities/Instrutor.java
+++ academia/src/main/java/com/residencia/academia/entities/Instrutor.java
@@ -1,5 +1,57 @@
 package com.residencia.academia.entities;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property = "codigoAutor",
+		scope = Instrutor.class
+)
+@Entity
+@Table(name = "instrutor")
 public class Instrutor {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigoinstrutor")
+	private Integer codigoInstrutor;
+	
+	@Column(name = "rginstrutor")
+	private Integer rgInstrutor;
+	
+	@Column(name = "nomeinstrutor")
+	private String nomeInstrutor;
+
+	public Integer getCodigoInstrutor() {
+		return codigoInstrutor;
+	}
+
+	public void setCodigoInstrutor(Integer codigoInstrutor) {
+		this.codigoInstrutor = codigoInstrutor;
+	}
+
+	public Integer getRgInstrutor() {
+		return rgInstrutor;
+	}
+
+	public void setRgInstrutor(Integer rgInstrutor) {
+		this.rgInstrutor = rgInstrutor;
+	}
+
+	public String getNomeInstrutor() {
+		return nomeInstrutor;
+	}
+
+	public void setNomeInstrutor(String nomeInstrutor) {
+		this.nomeInstrutor = nomeInstrutor;
+	}
+	
+	
 }
--- academia/src/main/java/com/residencia/academia/entities/Telefone.java
+++ academia/src/main/java/com/residencia/academia/entities/Telefone.java
@@ -1,5 +1,57 @@
 package com.residencia.academia.entities;
 
-public class Telefone {
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property = "codigoTelefone",
+		scope = Telefone.class
+)
+@Entity
+@Table(name = "telefone")
+public class Telefone {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigotelefone")
+	private Integer codigoTelefone;
+	
+	@Column(name = "codigoinstrutor")
+	private Integer codigoInstrutor;
+	
+	@Column(name = "numerotelefone")
+	private Integer numeroTelefone;
+
+	public Integer getCodigoTelefone() {
+		return codigoTelefone;
+	}
+
+	public void setCodigoTelefone(Integer codigoTelefone) {
+		this.codigoTelefone = codigoTelefone;
+	}
+
+	public Integer getCodigoInstrutor() {
+		return codigoInstrutor;
+	}
+
+	public void setCodigoInstrutor(Integer codigoInstrutor) {
+		this.codigoInstrutor = codigoInstrutor;
+	}
+
+	public Integer getNumeroTelefone() {
+		return numeroTelefone;
+	}
+
+	public void setNumeroTelefone(Integer numeroTelefone) {
+		this.numeroTelefone = numeroTelefone;
+	}
+	
 }
