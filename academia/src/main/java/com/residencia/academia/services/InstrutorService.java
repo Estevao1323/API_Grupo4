bb9b26a0471f419d105d7c0d1e3f511180097bbd
Rian-Fernandes <rian10fernandes@hotmail.com>
2023-10-11 22:12:52 -0300

    Criando controler, service e repository

--- academia/src/main/java/com/residencia/academia/services/InstrutorService.java
+++ academia/src/main/java/com/residencia/academia/services/InstrutorService.java
@@ -1,5 +1,52 @@
 package com.residencia.academia.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.residencia.academia.entities.Instrutor;
+import com.residencia.academia.repositories.InstrutorRepository;
+
+@Service
 public class InstrutorService {
 
+	@Autowired
+	InstrutorRepository instrutorRepo;
+
+	public List<Instrutor> listarInstrutor() {
+		return instrutorRepo.findAll();
+	}
+
+	public Instrutor buscarInstrutorPorId(Integer id) {
+		return instrutorRepo.findById(id).orElse(null);
+	}
+
+	public Instrutor salvarInstrutor(Instrutor instrutor) {
+		return instrutorRepo.save(instrutor);
+	}
+
+	public Instrutor atualizarInstrutor(Instrutor instrutor) {
+		return instrutorRepo.save(instrutor);
+	}
+
+	public Boolean deletarInstrutor(Instrutor instrutor) {
+		if (instrutor == null)
+			return false;
+
+		Instrutor instrutorExistente = buscarInstrutorPorId(instrutor.getCodigoInstrutor());
+
+		if (instrutorExistente == null)
+			return false;
+
+		instrutorRepo.delete(instrutor);
+
+		Instrutor instrutorContinuaExistindo = buscarInstrutorPorId(instrutor.getCodigoInstrutor());
+
+		if (instrutorContinuaExistindo == null)
+			return true;
+
+		return false;
+
+	}
 }
